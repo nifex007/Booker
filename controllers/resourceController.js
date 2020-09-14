9c9b96ddba7b82416b4328f64917e9b7183d5ce3
Nifemi Sola-Ojo <nifex007@yahoo.com>
2020-09-14 18:34:21 +0100

    Update Resource endpoint - Controller - Route

--- controllers/resourceController.js
+++ controllers/resourceController.js
@@ -1,5 +1,6 @@
 const ResourceModel = require('../models/Resource');
 const asyncHandler = require('../middlewares/async');
+const ErrorResponse = require('../utilities/errorResponse');
 /**
  * Create new Resource
  * POST /api/v1/resource
@@ -16,3 +17,19 @@ exports.createResource = asyncHandler(async (request, response, next) => {
 });
 
 
+/**
+ * Update a specific Resource
+ * POST /api/v1/resource/:id
+ * @access Private
+ */
+ exports.updateResource = asyncHandler(async (request, response, next) => {
+     const resource = await ResourceModel.findByIdAndUpdate(request.params.id, request.body, {new: true, runValidators: true});
+     if (!resource) {
+         return next(new ErrorResponse(`Resource with id of ${request.params.id} not found`, 404));
+     }
+     response.status(200).json({
+         success: true,
+         message: "OK",
+         data: resource
+     });
+ });
--- routes/resources.js
+++ routes/resources.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const { createResource } = require('../controllers/resourceController');
+const { createResource, updateResource } = require('../controllers/resourceController');
 
 const router = express.Router();
 
 router.route('/resource').post(createResource);
+router.route('/resource/:id').put(updateResource);
 
 
 module.exports = router;
